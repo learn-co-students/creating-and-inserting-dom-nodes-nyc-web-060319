e6567ac569d32908781873004e1a3da287218a39
github username <github email address>
2019-07-17 11:54:03 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div')
+
+element.innerHTML = 'Hello, DOM'
+
+document.body.appendChild(element)
+element.style.textAlign = 'left'
+
+var element2 = document.createElement('div')
+element2.innerHTML = "This is another element I am trying to add"
+
+element2.remove()
+
+
+// you can also do removeChild(element.querySelector('div')) --> if you have a child to remove within a parent element
